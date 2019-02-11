67090b8930f0bd8d59395f4ec05cdeb8455c072e
Johann Brehmer <mail@johannbrehmer.de>
2019-02-11 18:53:15 -0500

    Convolutional ratio estimator

--- inference/models.py
+++ inference/models.py
@@ -90,9 +90,12 @@ class DenseRatioEstimator(nn.Module):
 
 
 class Conv2DRatioEstimator(nn.Module):
-    def __init__(self, resolution, n_conv_layers=3, n_dense_layers=2, n_feature_maps=10, kernel_size=5, pooling_size=2, n_hidden_dense=100, activation="relu"):
+    def __init__(self, n_parameters, resolution, n_conv_layers=3, n_dense_layers=2, n_feature_maps=10, kernel_size=5, pooling_size=2,
+                 n_hidden_dense=100, activation="relu"):
 
         super(Conv2DRatioEstimator, self).__init__()
+
+        assert n_conv_layers > 0
 
         # Save input
         self.resolution = resolution
@@ -103,110 +106,80 @@ class Conv2DRatioEstimator(nn.Module):
         self.activation = get_activation_function(activation)
 
         # Build network
-        self.layers = nn.ModuleList()
+        self.parameter_layer = None
+        self.conv_layers = nn.ModuleList()
+        self.n_dense_layers = nn.ModuleList()
 
         last_channels = 1
         current_size = resolution
-        
-        # TODO: theta input
+
+        # Initial transformations of parameters
+        self.param_layer = nn.Linear(n_parameters, n_feature_maps)
 
         # Convolutional and pooling layers
-        for i_conv in range(n_conv_layers):
-            self.layers.append(
+        for i_conv in range(n_conv_layers - 1):
+            self.conv_layers.append(
                 nn.Conv2d(
                     in_channels=last_channels,
                     out_channels=n_feature_maps,
                     kernel_size=kernel_size,
                     stride=1,
                     padding=int(kernel_size - 1 / 2),
-                    bias=True
+                    bias=(i_conv > 0),
                 )
             )
-            self.layers.append(self.activation)
-            self.layers.append(
-                nn.MaxPool2d(kernel_size=pooling_size,stride=pooling_size)
-            )
+            self.conv_layers.append(nn.BatchNorm2d(n_feature_maps))
+            self.conv_layers.append(self.activation)
+            self.conv_layers.append(nn.MaxPool2d(kernel_size=pooling_size, stride=pooling_size))
 
             last_channels = n_feature_maps
             current_size /= pooling_size
 
-        n_units = last_channels * current_size * current_size
+        n_units = last_channels * current_size * current_size + n_parameters
 
         # Fully connected layers
-        for i_conv in range(n_conv_layers - 1):
-            self.layers.append(
-                nn.Conv2d(
-                    in_channels=last_channels,
-                    out_channels=n_feature_maps,
-                    kernel_size=kernel_size,
-                    stride=1,
-                    padding=int(kernel_size - 1 / 2),
-                    bias=True
-                )
+        for i_conv in range(n_dense_layers - 1):
+            self.dense_layers.append(
+                nn.Linear(n_units, n_hidden_dense)
             )
-            self.layers.append(nn.Linear(n_units, n_hidden_dense))
-            self.layers.append(nn.ReLU)
+            self.dense_layers.append(nn.Linear(n_units, n_hidden_dense))
+            self.dense_layers.append(nn.ReLU)
 
             n_units = n_hidden_dense
 
         # Log r layer
-        self.layers.append(nn.Linear(n_units, 1))
-
+        self.dense_layers.append(nn.Linear(n_units, 1))
 
     def forward(self, theta, x, track_score=True, return_grad_x=False, create_gradient_graph=True):
-
-        """ Calculates estimated log likelihood ratio and the derived score. """
-
-        # Track gradient wrt theta
+        # Track gradients
         if track_score and not theta.requires_grad:
             theta.requires_grad = True
 
-        # Track gradient wrt x
         if return_grad_x and not x.requires_grad:
             x.requires_grad = True
 
-        # log r estimator
-        log_r_hat = torch.cat((theta, x), 1)
+        # Convolutional and pooling layers
+        h = x
+        for i_layer, conv_layer in enumerate(self.conv_layers):
+            h = conv_layer(h)
+            if i_layer == 0:
+                h = h + self.param_layer(theta).unsqueeze(2).unsqueeze(3)
 
-        for i, layer in enumerate(self.layers):
-            if i > 0:
-                log_r_hat = self.activation(log_r_hat)
-            log_r_hat = layer(log_r_hat)
+        # Dense layers
+        h = h.reshape(h.size(0), -1)
+        h = torch.cat((h, theta), 1)
+        for dense_layer in self.dense_layers:
+            h = dense_layer(h)
 
-        # Bayes-optimal s
-        s_hat = 1.0 / (1.0 + torch.exp(log_r_hat))
-
-        # Score t
-        if track_score:
-            t_hat, = grad(
-                log_r_hat,
-                theta,
-                grad_outputs=torch.ones_like(log_r_hat.data),
-                # grad_outputs=log_r_hat.data.new(log_r_hat.shape).fill_(1),
-                only_inputs=True,
-                create_graph=create_gradient_graph,
-            )
-        else:
-            t_hat = None
-
-        # Calculate gradient wrt x
-        if return_grad_x:
-            x_gradient, = grad(
-                log_r_hat,
-                x,
-                grad_outputs=torch.ones_like(log_r_hat.data),
-                only_inputs=True,
-                create_graph=create_gradient_graph,
-            )
-
-            return s_hat, log_r_hat, t_hat, x_gradient
-
-        return s_hat, log_r_hat, t_hat
+        return h
 
     def to(self, *args, **kwargs):
         self = super(Conv2DRatioEstimator, self).to(*args, **kwargs)
 
-        for i, layer in enumerate(self.layers):
-            self.layers[i] = layer.to(*args, **kwargs)
+        for i, layer in enumerate(self.conv_layers):
+            self.conv_layers[i] = layer.to(*args, **kwargs)
+        for i, layer in enumerate(self.dense_layers):
+            self.dense_layers[i] = layer.to(*args, **kwargs)
+        self.param_layer = self.param_layer.to(*args, **kwargs)
 
         return self
