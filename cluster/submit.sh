4530f4ea493ea2ee3944f9c0d1628707eda56893
Johann Brehmer <mail@johannbrehmer.de>
2019-03-18 15:20:52 -0400

    Prepared cluster scripts for training

--- cluster/submit.sh
+++ cluster/submit.sh
@@ -4,3 +4,16 @@ cd /scratch/jb6504/StrongLensing-Inference/cluster
 
 sbatch --array=0-99 simulate_train.sh
 sbatch --array=0-9 simulate_test.sh
+
+# sbatch combine_samples.sh
+
+# sbatch train_carl.sh
+# sbatch train_carl_log.sh
+# sbatch train_alice.sh
+# sbatch train_alice_log.sh
+# sbatch train_alices_alpha_0.1.sh
+# sbatch train_alices_log_alpha_0.1.sh
+# sbatch train_alices_alpha_1.sh
+# sbatch train_alices_log_alpha_1.sh
+# sbatch train_alices_alpha_10.sh
+# sbatch train_alices_log_alpha_10.sh
---
+++ cluster/train_alices_log_alpha_0.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_alices_log_alpha_0.1
+#SBATCH --output=log_train_alices_log_alpha_0.1.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:1
+
+source activate lensing
+cd /scratch/jb6504/StrongLensing-Inference/
+
+python -u train.py alices --log --alpha 0.1 --name alices_log_alpha_0.1 --dir /scratch/jb6504/StrongLensing-Inference
