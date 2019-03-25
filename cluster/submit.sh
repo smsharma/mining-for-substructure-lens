675fc4dcf8a1b1e663f3dcf1f142f65c7fec5ba7
Johann <mail@johannbrehmer.de>
2019-03-25 16:40:53 -0400

    Updated submit.sh

--- cluster/submit.sh
+++ cluster/submit.sh
@@ -2,15 +2,16 @@
 
 cd /scratch/jb6504/StrongLensing-Inference/cluster
 
-sbatch --array=0-99 simulate_train.sh
+# sbatch --array=0-99 simulate_train.sh
 # sbatch --array=0-9 simulate_test.sh
 
 # sbatch combine_samples.sh
 
-# sbatch train_carl.sh
-# sbatch train_carl_log.sh
-# sbatch train_alice.sh
-# sbatch train_alice_log.sh
+sbatch train_debug.sh
+sbatch train_carl.sh
+sbatch train_carl_log.sh
+sbatch train_alice.sh
+sbatch train_alice_log.sh
 # sbatch train_alices.sh
 # sbatch train_alices_log.sh
 # sbatch train_alices_smallalpha.sh
