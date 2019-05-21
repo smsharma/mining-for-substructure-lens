e965f56f277afe759c11b47e821589c1b3fb5872
Johann Brehmer <mail@johannbrehmer.de>
2019-05-21 14:03:37 -0400

    Local calibration

--- cluster/calibrate.sh
+++ cluster/calibrate.sh
@@ -8,13 +8,16 @@
 #SBATCH --time=7-00:00:00
 #SBATCH --gres=gpu:1
 
+DIR=/Users/johannbrehmer/work/projects/strong_lensing/StrongLensing-Inference
+# DIR=/scratch/jb6504/StrongLensing-Inference/
+
 source activate lensing
-cd /scratch/jb6504/StrongLensing-Inference/
+cd $DIR
 
-python -u calibrate.py carl_aux_grid carl_aux_calibrate --dir /scratch/jb6504/StrongLensing-Inference
-python -u calibrate.py alice_aux_grid alice_aux_calibrate --dir /scratch/jb6504/StrongLensing-Inference
-python -u calibrate.py alices_aux_grid alices_aux_calibrate--dir /scratch/jb6504/StrongLensing-Inference
+python -u calibrate.py carl_aux_grid carl_aux_calibrate --dir $DIR
+python -u calibrate.py alice_aux_grid alice_aux_calibrate --dir $DIR
+python -u calibrate.py alices_aux_grid alices_aux_calibrate --dir $DIR
 
-python -u calibrate.py carl_pointref_aux_grid carl_pointref_aux_calibrate --dir /scratch/jb6504/StrongLensing-Inference
-python -u calibrate.py alice_pointref_aux_grid alice_pointref_aux_calibrate --dir /scratch/jb6504/StrongLensing-Inference
-python -u calibrate.py alices_pointref_aux_grid alices_pointref_aux_calibrate--dir /scratch/jb6504/StrongLensing-Inference
+python -u calibrate.py carl_pointref_aux_grid carl_pointref_aux_calibrate --dir $DIR
+python -u calibrate.py alice_pointref_aux_grid alice_pointref_aux_calibrate --dir $DIR
+python -u calibrate.py alices_pointref_aux_grid alices_pointref_aux_calibrate --dir $DIR
