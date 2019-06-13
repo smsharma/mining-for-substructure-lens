e9af7b14f82f08413ffc57516fa684398f16e182
Johann Brehmer <mail@johannbrehmer.de>
2019-06-13 08:01:03 -0400

    Prepared evaluation and training scripts

--- cluster/submit.sh
+++ cluster/submit.sh
@@ -7,7 +7,7 @@ cd /scratch/jb6504/StrongLensing-Inference/cluster
 # Simulation
 ############################################################
 
-sbatch --array=0-99 simulate_train.sh
+# sbatch --array=0-99 simulate_train.sh
 # sbatch --array=0-624 simulate_calibration.sh
 # sbatch simulate_calibration_ref.sh
 # sbatch --array=0-9 simulate_test.sh
@@ -24,9 +24,18 @@ sbatch --array=0-99 simulate_train.sh
 # Training
 ############################################################
 
-# sbatch train_carl.sh
-# sbatch train_alice.sh
-# sbatch train_alices.sh
+sbatch train_carl.sh
+sbatch train_alice.sh
+sbatch train_alices.sh
+
+
+############################################################
+# Evaluation
+############################################################
+
+sbatch eval_carl.sh
+sbatch eval_alice.sh
+sbatch eval_alices.sh
 
 
 ############################################################
@@ -34,12 +43,3 @@ sbatch --array=0-99 simulate_train.sh
 ############################################################
 
 # sbatch calibrate.sh
-
-
-############################################################
-# Evaluation
-############################################################
-
-# sbatch eval_carl.sh
-# sbatch eval_alice.sh
-# sbatch eval_alices.sh
