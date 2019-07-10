5f6ad9c8848162e7c382aaca20b28d8efbdef2f1
Johann Brehmer <mail@johannbrehmer.de>
2019-07-10 16:17:28 -0400

    More changes to calibration scripts

--- cluster/simulate_calibration_ref.sh
+++ cluster/simulate_calibration_ref.sh
@@ -11,4 +11,7 @@
 source activate lensing
 cd /scratch/jb6504/StrongLensing-Inference/
 
-python -u simulate.py -n 10000 --calref --dir /scratch/jb6504/StrongLensing-Inference
+python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} --fixz --fixm --fixalign -n 5000 --name calibrate_fix_ref --dir /scratch/jb6504/recycling_strong_lensing
+python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} --fixz --fixalign -n 5000 --name calibrate_mass_ref --dir /scratch/jb6504/recycling_strong_lensing
+python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} --fixz --fixm -n 5000 --name calibrate_align_ref --dir /scratch/jb6504/recycling_strong_lensing
+python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} -n 5000 --name calibrate_full_ref --dir /scratch/jb6504/recycling_strong_lensing
