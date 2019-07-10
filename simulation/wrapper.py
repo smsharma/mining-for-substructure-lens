b574a3e18554acb83e672991095c9308c82ffbab
Johann Brehmer <mail@johannbrehmer.de>
2019-07-10 13:12:46 -0400

    Rewrote wrapper for recycling

--- simulation/wrapper.py
+++ simulation/wrapper.py
@@ -12,13 +12,14 @@ logger = logging.getLogger(__name__)
 def augmented_data(
     f_sub=None,
     beta=None,
+    f_sub_alt=None,
+    beta_alt=None,
     f_sub_ref=None,
     beta_ref=None,
     f_sub_prior=uniform(0.001, 0.199),
     beta_prior=uniform(-2.5, 1.0),
     n_images=None,
     n_thetas_marginal=1000,
-    inverse=False,
     draw_host_mass=True,
     draw_host_redshift=True,
     draw_alignment=True,
@@ -34,25 +35,20 @@ def augmented_data(
         n_images = len(f_sub)
     n_verbose = max(1, n_images // 100)
 
-    # Test hypothesis
-    if f_sub is None:
-        f_sub = f_sub_prior.rvs(size=n_images)
-    if beta is None:
-        beta = beta_prior.rvs(size=n_images)
-    f_sub = np.clip(f_sub, 1.0e-6, None)
-    beta = np.clip(beta, None, -1.01)
+    # Hypothesis for sampling
+    beta, f_sub = _draw_params(beta, beta_prior, f_sub, f_sub_prior, n_images)
+
+    # Alternate hypothesis (test hypothesis when swapping num - den)
+    beta_alt, f_sub_alt = _draw_params(beta_alt, beta_prior, f_sub_alt, f_sub_prior, n_images)
 
     # Reference hypothesis
-    if f_sub_ref is None:
-        f_sub_ref = f_sub_prior.rvs(size=n_thetas_marginal)
-    if beta_ref is None:
-        beta_ref = beta_prior.rvs(size=n_thetas_marginal)
-    f_sub_ref = np.clip(f_sub_ref, 1.0e-6, None)
-    beta_ref = np.clip(beta_ref, None, -1.01)
+    beta_ref, f_sub_ref = _draw_params(beta_alt, beta_prior, f_sub_alt, f_sub_prior, n_thetas_marginal - 1)
     params_ref = np.vstack((f_sub_ref, beta_ref)).T
 
     # Output
-    all_params, all_images, all_t_xz, all_log_r_xz, all_sub_latents, all_global_latents = [], [], [], [], [], []
+    all_params, all_params_alt, all_images = [], [], []
+    all_t_xz, all_t_xz_alt, all_log_r_xz, all_log_r_xz_alt = [], [], [], []
+    all_sub_latents, all_global_latents = [], []
     all_dx_dm = []
 
     # Main loop
@@ -65,17 +61,14 @@ def augmented_data(
         # Prepare params
         this_f_sub = _pick_param(f_sub, i_sim, n_images)
         this_beta = _pick_param(beta, i_sim, n_images)
+        this_f_sub_alt = _pick_param(f_sub_alt, i_sim, n_images)
+        this_beta_alt = _pick_param(beta_alt, i_sim, n_images)
+
         params = np.asarray([this_f_sub, this_beta]).reshape((1, 2))
-        params_eval = np.vstack((params, params_ref)) if mine_gold else None
+        params_alt = np.asarray([this_f_sub_alt, this_beta_alt]).reshape((1, 2))
+        params_eval = np.vstack((params, params_alt, params_ref)) if mine_gold else None
 
         logger.debug("Numerator hypothesis: f_sub = %s, beta = %s", this_f_sub, this_beta)
-
-        if inverse:
-            # Choose one theta from prior that we use for sampling here
-            i_sample = np.random.randint(n_thetas_marginal)
-            this_f_sub, this_beta = params_ref[i_sample]
-
-        logger.debug("Running simulation at f_sub = %s, beta = %s", this_f_sub, this_beta)
 
         if mine_gold:
             logger.debug("Evaluating joint log likelihood at %s", params_eval)
@@ -123,37 +116,53 @@ def augmented_data(
         global_latents = np.asarray(global_latents)
 
         all_params.append(params)
+        all_params_alt.append(params_alt)
         all_images.append(sim.image_poiss_psf)
         all_sub_latents.append(sub_latents)
         all_global_latents.append(global_latents)
 
         if mine_gold:
-            log_r_xz, uncertainty = _extract_log_r(sim, n_thetas_marginal)
-            logger.debug("log r(x,z) = %s +/- %s", log_r_xz, uncertainty)
-            if uncertainty > 0.1:
-                logger.debug("Large uncertainty: log r(x,z) = %s +/- %s", log_r_xz, uncertainty)
-            all_t_xz.append(sim.joint_score)
-            logger.debug("t(x,z) = %s", sim.joint_score)
+            log_r_xz, log_r_xz_alt = _extract_log_r(sim, n_thetas_marginal)
             all_log_r_xz.append(log_r_xz)
+            all_log_r_xz_alt.append(log_r_xz_alt)
+
+            all_t_xz.append(sim.joint_scores[0])
+            all_t_xz_alt.append(sim.joint_scores[1])
+            logger.debug("t(x,z) = %s", sim.joint_score)
 
     if calculate_dx_dm and return_dx_dm:
         return (
             np.array(all_params).reshape((-1, 2)),
+            np.array(all_params_alt).reshape((-1, 2)),
             np.array(all_images),
             np.array(all_t_xz) if mine_gold else None,
+            np.array(all_t_xz_alt) if mine_gold else None,
             np.array(all_log_r_xz) if mine_gold else None,
+            np.array(all_log_r_xz_alt) if mine_gold else None,
             all_sub_latents,
             np.array(all_global_latents),
-            all_dx_dm
         )
     return (
         np.array(all_params).reshape((-1, 2)),
+        np.array(all_params_alt).reshape((-1, 2)),
         np.array(all_images),
         np.array(all_t_xz) if mine_gold else None,
+        np.array(all_t_xz_alt) if mine_gold else None,
         np.array(all_log_r_xz) if mine_gold else None,
+        np.array(all_log_r_xz_alt) if mine_gold else None,
         all_sub_latents,
         np.array(all_global_latents),
     )
+
+
+def _draw_params(beta, beta_prior, f_sub, f_sub_prior, n_images):
+    if f_sub is None:
+        f_sub = f_sub_prior.rvs(size=n_images)
+    if beta is None:
+        beta = beta_prior.rvs(size=n_images)
+    f_sub = np.clip(f_sub, 1.0e-6, None)
+    beta = np.clip(beta, None, -1.01)
+    return beta, f_sub
 
 
 def _pick_param(xs, i, n):
@@ -164,27 +173,8 @@ def _pick_param(xs, i, n):
         return xs
 
 
-def _extract_log_r(sim, n_thetas_marginal):
-    # Just a reference point?
-    if n_thetas_marginal == 1:
-        log_r_xz = sim.joint_log_probs[0] - sim.joint_log_probs[1]
-        return log_r_xz, 0.0
-
-    # Evaluate likelihood ratio wrt evidence
-    delta_log = np.asarray(sim.joint_log_probs[1:] - sim.joint_log_probs[0] - np.log(float(n_thetas_marginal)), dtype=np.float128)
+def _extract_log_r(sim, i, n_thetas_marginal):
+    log_p_xz_from_marginal = np.delete(sim.joint_log_probs, i, axis=0)
+    delta_log = np.asarray(log_p_xz_from_marginal - sim.joint_log_probs[i] - np.log(float(n_thetas_marginal)), dtype=np.float128)
     log_r_xz = -1.0 * scipy.special.logsumexp(delta_log)
-
-    if not np.isfinite(log_r_xz):
-        logger.warning("Infinite log r for delta_log = %s", delta_log)
-
-    # Estimate uncertainty of log r from MC sampling
-    inverse_r_xz = np.exp(-log_r_xz)
-    inverse_r_xz_uncertainty = 0.0
-    for i_theta in range(n_thetas_marginal):
-        log_r_contribution = sim.joint_log_probs[i_theta + 1] - sim.joint_log_probs[0]
-        inverse_r_contribution = np.exp(log_r_contribution)
-        inverse_r_xz_uncertainty += (inverse_r_contribution - inverse_r_xz) ** 2.0
-    inverse_r_xz_uncertainty /= float(n_thetas_marginal) * (float(n_thetas_marginal) - 1.0)
-    log_r_xz_uncertainty = inverse_r_xz_uncertainty / inverse_r_xz
-
-    return log_r_xz, log_r_xz_uncertainty
+    return log_r_xz
