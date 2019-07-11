8dc0f4d66bc59d0c06fa1dcf8cdd61a3ea15f673
Johann <mail@johannbrehmer.de>
2019-07-11 09:46:15 -0400

    Fixed simulation script

--- cluster/simulate_train.sh
+++ cluster/simulate_train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=sim-tr
+#SBATCH --job-name=slr-s-tr
 #SBATCH --output=log_simulate_train_%a.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16GB
+#SBATCH --mem=32GB
 #SBATCH --time=7-00:00:00
-# #SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1
 
 source activate lensing
-cd /scratch/jb6504/StrongLensing-Inference/
+cd /scratch/jb6504/recycling_strong_lensing/
 
 python -u simulate.py --fixz --fixm --fixalign -n 10000 --name train_fix_${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/recycling_strong_lensing
 python -u simulate.py --fixz --fixalign -n 10000 --name train_mass_${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/recycling_strong_lensing
