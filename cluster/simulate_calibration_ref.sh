57e2ab7c478e9f3f0a8b8c0399a5995eda8c67d3
Johann <mail@johannbrehmer.de>
2019-07-29 11:07:55 -0400

    Fixed simulate_calibration_ref.sh

--- cluster/simulate_calibration_ref.sh
+++ cluster/simulate_calibration_ref.sh
@@ -11,7 +11,7 @@
 source activate lensing
 cd /scratch/jb6504/StrongLensing-Inference/
 
-python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} --fixz --fixm --fixalign -n 5000 --name calibrate_fix_ref --dir /scratch/jb6504/recycling_strong_lensing
-python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} --fixz --fixalign -n 5000 --name calibrate_mass_ref --dir /scratch/jb6504/recycling_strong_lensing
-python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} --fixz --fixm -n 5000 --name calibrate_align_ref --dir /scratch/jb6504/recycling_strong_lensing
-python -u simulate.py --calref --theta ${SLURM_ARRAY_TASK_ID} -n 5000 --name calibrate_full_ref --dir /scratch/jb6504/recycling_strong_lensing
+python -u simulate.py --calref --fixz --fixm --fixalign -n 5000 --name calibrate_fix_ref --dir /scratch/jb6504/StrongLensing-Inference
+python -u simulate.py --calref --fixz --fixalign -n 5000 --name calibrate_mass_ref --dir /scratch/jb6504/StrongLensing-Inference
+python -u simulate.py --calref --fixz --fixm -n 5000 --name calibrate_align_ref --dir /scratch/jb6504/StrongLensing-Inference
+python -u simulate.py --calref -n 5000 --name calibrate_full_ref --dir /scratch/jb6504/StrongLensing-Inference
