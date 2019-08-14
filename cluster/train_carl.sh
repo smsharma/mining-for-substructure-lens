bac7b9d9f619d094087b2b3d91e43832c585fedc
Johann Brehmer <mail@johannbrehmer.de>
2019-08-14 13:34:15 -0400

    Fixed typo

--- cluster/train_carl.sh
+++ cluster/train_carl.sh
@@ -12,7 +12,7 @@ source activate lensing
 base=/scratch/jb6504/recycling_strong_lensing/
 cd $base/
 
-python -u train.py carl train_fix carl_fix --dir $base-Inference
+python -u train.py carl train_fix carl_fix --dir $base
 python -u train.py carl train_mass carl_mass --load carl_fix --dir $base
 python -u train.py carl train_align carl_align --load carl_fix --dir $base
 python -u train.py carl train_full carl_full --load carl_fix --dir $base
