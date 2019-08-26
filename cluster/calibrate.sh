1c1008adeffd89dbad3ecef73748f83267e11856
Johann Brehmer <mail@johannbrehmer.de>
2019-08-26 15:32:36 -0400

    Updated to Sec 3, including way too many LFI references

--- cluster/calibrate.sh
+++ cluster/calibrate.sh
@@ -13,8 +13,8 @@ base=/scratch/jb6504/recycling_strong_lensing/
 cd $base
 
 python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 20 --name 20bins
-python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 30 --name 40bins
-python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 50 --name 60bins
+python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 40 --name 40bins
+python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 60 --name 60bins
 python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 20 -s --name 20sbins
-python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 30 -s --name 40sbins
-python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 50 -s --name 60sbins
+python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 40 -s --name 40sbins
+python -u calibrate.py alices_full_grid alices_calibrate_full --dir $base --bins 60 -s --name 60sbins
