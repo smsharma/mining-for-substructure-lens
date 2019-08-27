f339533b4da78d8c7e533bbb7836a6de58a4694c
Johann Brehmer <mail@johannbrehmer.de>
2019-08-27 08:04:02 -0400

    Changes to calibration data processing script

--- cluster/move_calibration_data.py
+++ cluster/move_calibration_data.py
@@ -5,7 +5,6 @@ import shutil
 BASE = "/scratch/jb6504/recycling_strong_lensing/data/samples/calibration/"
 FILENAME_NEW = "x_calibrate_full_theta_{}_updated.npy"
 FILENAME_OLD = "x_calibrate_full_theta_{}.npy"
-FILENAME_OUT = "x_calibrate_full_theta_{}_out.npy"
 
 
 def check(filename):
@@ -31,17 +30,11 @@ for i in range(625):
         print("Little data for i = {}: {}, {}".format(i, n_old, n_new))
 
     if n_new > n_old:
-        print("n {}".format(n_new))
-        source = FILENAME_NEW.format(i)
-    else:
-        print("o {}".format(n_old))
-        source = FILENAME_OLD.format(i)
-
-    try:
-        shutil.copy(BASE + source, BASE + FILENAME_OLD.format(i))
-    except IOError as e:
-        print("Unable to copy file. %s" % e)
-    except:
-        print("Unexpected error:", sys.exc_info())
+        try:
+            shutil.copy(BASE + FILENAME_NEW.format(i), BASE + FILENAME_OLD.format(i))
+        except IOError as e:
+            print("Unable to copy file. %s" % e)
+        except:
+            print("Unexpected error:", sys.exc_info())
 
 print("Done!")
